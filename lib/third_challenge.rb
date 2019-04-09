58f5abd94828a81ed5d0f5d9705cdf5a630708d1
Caleb Einarr <earthctzn1@gmail.com>
2019-04-09 00:35:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,8 +9,8 @@ def third_challenge
         },
       :hero_friends => []
    }, 
-   :capulet => {,
-      :patriarch => {:name => , :age => 
+   :capulet => {
+      :patriarch => {:name => , :age =>, 
         #your key/value pairs here
         },
       :matriarch => {:name => , :age => ,
