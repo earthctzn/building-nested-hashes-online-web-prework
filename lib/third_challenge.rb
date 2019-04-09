66465599237600b23c30d39f6730fad25cb157a2
Caleb Einarr <earthctzn1@gmail.com>
2019-04-09 00:35:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "51", 
         },
-      :matriarch => {:name => , :age => ,
+      :matriarch => {:name => "Lady Capulet", :age => ,
         #your key/value pairs here
         },
       :heroine => {:name => , :age => ,
